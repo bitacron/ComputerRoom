helloWorldchn/crms-server

feat: 使用SpringSecurity与RABC重构用户系统

src/main/java/com/example/room/acl/mapper/RoleMapper.java
@@ -1,0 +1,16 @@
+package com.example.room.acl.mapper;
+
+import com.example.room.acl.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}

bitacron/ComputerRoom

src/main/java/com/example/room/access/mapper/RoleMapper.java
@@ -1,0 +1,16 @@
+package com.example.room.access.mapper;
+
+import com.example.room.access.entity.Role;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhmy
+ * @since 2020-01-12
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}